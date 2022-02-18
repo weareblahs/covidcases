9b982116ba130bf07452898ed238fdaf74a92b55
thisistyx-AUTO <tanyuxuan2005@gmail.com>
2022-02-18 22:25:53 +0800

    Remove notes from update.cmd

--- update.cmd
+++ update.cmd
@@ -1,14 +1,11 @@
 @echo off
-# Pre-update stuffs
+
 del cases
 del index.html
 copy index_temp.html index.html
 
-# Start download data
 curl -o cases "https://covid-19.samsam123.name.my/api/cases?date=latest"
 
-# Import cases
 for /f "delims=" %%i in ('backend\jsonextractor cases cases_new') do set cases_new=%%i >nul
 
-# Replace data?
 fart index.html "No data" %cases_new%
