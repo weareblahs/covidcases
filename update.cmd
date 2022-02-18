f8f9e1ebd75e676221ab32eff294bc7d5ceaf007
thisistyx-AUTO <tanyuxuan2005@gmail.com>
2022-02-18 22:31:20 +0800

    Updated

--- update.cmd
+++ update.cmd
@@ -9,3 +9,7 @@ curl -o cases "https://covid-19.samsam123.name.my/api/cases?date=latest"
 for /f "delims=" %%i in ('backend\jsonextractor cases cases_new') do set cases_new=%%i >nul
 
 fart index.html "No data" %cases_new%
+
+git add .
+git commit -m "Updated"
+git push
