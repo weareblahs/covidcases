cdeb220a48c669780f9561bf4fd8389c873cf59c
thisistyx-AUTO <tanyuxuan2005@gmail.com>
2022-02-18 22:44:43 +0800

    Changed stuffs + add jq + change cmd stuffs

--- update.cmd
+++ update.cmd
@@ -6,10 +6,10 @@ copy index_temp.html index.html
 
 curl -o cases "https://covid-19.samsam123.name.my/api/cases?date=latest"
 
-for /f "delims=" %%i in ('backend\jsonextractor cases cases_new') do set cases_new=%%i >nul
+for /f "delims=" %%i in ('backend\jq .cases_new cases') do set cases_new=%%i >nul
 
-fart index.html "No data" %cases_new%
+backend\fart index.html "No data" %cases_new%
 
 git add .
-git commit -m "Updated"
+git commit -m "Updated %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2%"
 git push
