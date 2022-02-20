d59c9798e4d3ce36353542792f79d96a9d8fbf9d
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2022-02-20 15:15:08 +0800

    Remove ./ from bash
    jq is installed before every run on GitHub Actions

--- update.sh
+++ update.sh
@@ -1,5 +1,3 @@
-chmod +x ./jq
-
 rm cases
 rm index.html
 cp index_temp.html index.html
@@ -8,9 +6,9 @@ cp deaths/index_temp.html deaths/index.html
 echo Getting date...
 echo Grabbing the latest cases...
 curl -X GET -o cases https://covid-19.samsam123.name.my/api/cases?date=latest -H "User-Agent: weareblahs-covidcases/1.0.0" -H "Referer: https://github.com/weareblahs/covidcases"
-cases=$( ./jq -r ".cases_new" cases )
+cases=$( jq -r ".cases_new" cases )
 echo Getting date...
-originalcasedate=$( ./jq -r ".date" cases )
+originalcasedate=$( jq -r ".date" cases )
 replacedcasesdate=$( date -d "$originalcasedate" +"%d %B %Y")
 echo Replacing cases...
 sed -i "s/No data/$cases/g" index.html
@@ -18,9 +16,9 @@ sed -i "s/INSERT_DATE_HERE/$replacedcasesdate/g" index.html
 
 echo Grabbing latest deaths data...
 curl -X GET -o deathsdata https://covid-19.samsam123.name.my/api/death?date=latest -H "User-Agent: weareblahs-covidcases/1.0.0" -H "Referer: https://github.com/weareblahs/covidcases"
-deaths=$( ./jq -r ".deaths_new" deathsdata )
+deaths=$( jq -r ".deaths_new" deathsdata )
 echo Getting date...
-originaldeathsdate=$( ./jq -r ".date" deathsdata )
+originaldeathsdate=$( jq -r ".date" deathsdata )
 replaceddeathsdate=$( date -d "$originaldeathsdate" +"%d %B %Y")
 echo Replacing deaths...
 cd deaths
