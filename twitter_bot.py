fef5d9f52ce98ca444c2892f8f2c667a351a455d
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2022-03-08 21:36:40 +0800

    URL test

--- twitter_bot.py
+++ twitter_bot.py
@@ -5,6 +5,7 @@ consumera = os.environ["TWICK"]
 consumerb = os.environ["TWICS"]
 accessa = os.environ["TWIAT"]
 accessb = os.environ["TWIATS"]
+gitworkspace = os.environ["GITHUB_WORKSPACE"]
 
 auth = tweepy.OAuthHandler(
             consumera,
@@ -16,7 +17,7 @@ auth.set_access_token(
             )
 api = tweepy.API(auth)
 
-media = api.media_upload("$GITHUB_WORKSPACE/img/cases.png")
+media = api.media_upload(gitworkspace + "/img/cases.png")
  
 tweet = "Test tweet with latest cases"
 post_result = api.update_status(status=tweet, media_ids=[media.media_id])
