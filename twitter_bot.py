652d72479390d243840e62c6dc33c82fbedb8c21
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2022-03-08 19:18:06 +0800

    Twitter bot test

---
+++ twitter_bot.py
@@ -0,0 +1,17 @@
+import tweepy
+import os
+
+CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+ACCESS_SECRET = os.environ['TWITTER_ACCESS_SECRET']
+ACCESS_TOKEN_SECRET = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+
+client = tweepy.Client(consumer_key='CONSUMER_KEY',
+                       consumer_secret='CONSUMER_SECRET',
+                       access_token='ACCESS_SECRET',
+                       access_token_secret='TWITTER_ACCESS_TOKEN_SECRET')
+
+# Replace the text with whatever you want to Tweet about
+response = client.create_tweet(text="Tweet tweeted from GitHub Actions to ensure that it's working with all the stuffs. Disregard this tweet.")
+
+print(response)
