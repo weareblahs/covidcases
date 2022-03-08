2623b7953a3efb99d8c150efbdb11e8c602c9a7d
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2022-03-08 21:21:34 +0800

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,17 +1,17 @@
 import tweepy
 import os
 
-CONSUMER_KEY = os.environ['TWICK']
-CONSUMER_SECRET = os.environ['TWICS']
-ACCESS_SECRET = os.environ['TWIAT']
-ACCESS_TOKEN_SECRET = os.environ['TWIATS']
+consumera = os.environ["TWICK"]
+consumerb = os.environ["TWICS"]
+accessa = os.environ["TWIAT"]
+accessb = os.environ["TWIATS"]
 
-client = tweepy.Client(consumer_key='CONSUMER_KEY',
-                       consumer_secret='CONSUMER_SECRET',
-                       access_token='ACCESS_SECRET',
-                       access_token_secret='TWITTER_ACCESS_TOKEN_SECRET')
+client = tweepy.Client(consumer_key=consumera,
+                       consumer_secret=consumerb,
+                       access_token=accessa,
+                       access_token_secret=accessb
+                       )
 
-# Replace the text with whatever you want to Tweet about
-response = client.create_tweet(text="Tweet tweeted from GitHub Actions to ensure that it's working with all the stuffs. Disregard this tweet.")
+response = client.create_tweet(text="Test tweet from GitHub actions.")
 
 print(response)
