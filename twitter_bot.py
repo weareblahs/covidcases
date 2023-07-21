ff0e4e03f2a5b61971d22ca0a9d39555d3deb74b
weareblahs <tanyuxuan2005@gmail.com>
2023-07-22 01:29:05 +0800

    Fix media upload

--- twitter_bot.py
+++ twitter_bot.py
@@ -20,12 +20,23 @@ api = tweepy.Client(bearer_token=bearer,
                     consumer_key=consumera,
                     consumer_secret=consumerb)
 
+# Use old Twitter API for media upload (legacyapi)
+legacyauth = tweepy.OAuthHandler(
+            consumera,
+            consumerb
+             )
+legacyauth.set_access_token(
+             accessa,
+             accessb
+            )
+legacyapi = tweepy.API(legacyauth)
+
 os.chdir(gitworkspace)
 print("Uploading 1 of 17...")
 filenames = ['img/cases.png', 'img/deaths.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Malaysia as of " + showdate + ". Open this thread for state information. (1/17) \n See pinned tweet on where to find us after February 9. \n#COVID19 #COVID19Cases #CoronavirusCases"
 original_tweet = api.create_tweet(text=tweet, media_ids=media_ids)
@@ -34,7 +45,7 @@ print("Uploading 2 of 17...")
 filenames = ['img/cases_Johor.png', 'img/deaths_Johor.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Johor as of " + showdate + ". (2/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=original_tweet.id, auto_populate_reply_metadata=True)
@@ -43,7 +54,7 @@ print("Uploading 3 of 17...")
 filenames = ['img/cases_Kedah.png', 'img/deaths_Kedah.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Kedah as of " + showdate + ". (3/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -52,7 +63,7 @@ print("Uploading 4 of 17...")
 filenames = ['img/cases_Kelantan.png', 'img/deaths_Kelantan.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Kelantan as of " + showdate + ". (4/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -61,7 +72,7 @@ print("Uploading 5 of 17...")
 filenames = ['img/cases_Melaka.png', 'img/deaths_Melaka.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Melaka as of " + showdate + ". (5/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -70,7 +81,7 @@ print("Uploading 6 of 17...")
 filenames = ['img/cases_Negeri Sembilan.png', 'img/deaths_Negeri Sembilan.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Negeri Sembilan as of " + showdate + ". (6/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -79,7 +90,7 @@ print("Uploading 7 of 17...")
 filenames = ['img/cases_Pahang.png', 'img/deaths_Pahang.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Pahang as of " + showdate + ". (7/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -88,7 +99,7 @@ print("Uploading 8 of 17...")
 filenames = ['img/cases_Perak.png', 'img/deaths_Perak.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Perak as of " + showdate + ". (8/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -97,7 +108,7 @@ print("Uploading 9 of 17...")
 filenames = ['img/cases_Perlis.png', 'img/deaths_Perlis.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Perlis as of " + showdate + ". (9/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -106,7 +117,7 @@ print("Uploading 10 of 17...")
 filenames = ['img/cases_Pulau Pinang.png', 'img/deaths_Pulau Pinang.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Pulau Pinang as of " + showdate + ". (10/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -115,7 +126,7 @@ print("Uploading 11 of 17...")
 filenames = ['img/cases_Sabah.png', 'img/deaths_Sabah.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Sabah as of " + showdate + ". (11/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -124,7 +135,7 @@ print("Uploading 12 of 17...")
 filenames = ['img/cases_Sarawak.png', 'img/deaths_Sarawak.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Sarawak as of " + showdate + ". (12/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -133,7 +144,7 @@ print("Uploading 13 of 17...")
 filenames = ['img/cases_Selangor.png', 'img/deaths_Selangor.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Selangor as of " + showdate + ". (13/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -142,7 +153,7 @@ print("Uploading 14 of 17...")
 filenames = ['img/cases_Terengganu.png', 'img/deaths_Terengganu.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Terengganu as of " + showdate + ". (14/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -151,7 +162,7 @@ print("Uploading 15 of 17...")
 filenames = ['img/cases_Kuala Lumpur.png', 'img/deaths_Kuala Lumpur.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Kuala Lumpur as of " + showdate + ". (15/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -160,7 +171,7 @@ print("Uploading 16 of 17...")
 filenames = ['img/cases_Labuan.png', 'img/deaths_Labuan.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Labuan as of " + showdate + ". (16/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
@@ -169,7 +180,7 @@ print("Uploading 17 of 17...")
 filenames = ['img/cases_Putrajaya.png', 'img/deaths_Putrajaya.png']
 media_ids = []
 for filename in filenames:
-     res = api.media_upload(filename)
+     res = legacyapi.media_upload(filename)
      media_ids.append(res.media_id)
 tweet = "COVID-19 cases and deaths in Putrajaya as of " + showdate + ". (17/17)"
 reply_tweet = api.create_tweet(text=tweet, media_ids=media_ids, in_reply_to_status_id=reply_tweet.id, auto_populate_reply_metadata=True)
