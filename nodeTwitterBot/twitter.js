9591b875d50f581876174f2c893cbf2a8290edbc
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2025-04-30 11:12:00 +0800

    try/catch

--- nodeTwitterBot/twitter.js
+++ nodeTwitterBot/twitter.js
@@ -1,5 +1,5 @@
 import { getData } from "./getData.js";
-import { TwitterApi } from "twitter-api-v2";
+import { ApiResponseError, TwitterApi } from "twitter-api-v2";
 const postToTwitter = async () => {
   const data = await getData();
   let tweet1 = `COVID-19 cases as of ${data.date.toDateString()} (1/2):\n`;
@@ -25,16 +25,24 @@ const postToTwitter = async () => {
     `"COVID-19 cases as of ${data.date.toDateString()}" from:MYCOVIDCasesBot` // Test with data after 1 day
   );
 
-  if (jsTweets.data.meta.result_count != 0) {
-    console.log(
-      "Data exists on Twitter account MYCOVIDCasesBot. Skipping tweet proccess."
-    );
-  } else {
-    console.log("Tweeting tweet 1 of 2...");
-    await twitterClient.v2.tweet(tweet1);
-    console.log("Tweeting tweet 2 of 2...");
-    await twitterClient.v2.tweet(tweet2);
-    console.log("Successful");
+  try {
+    if (jsTweets.data.meta.result_count != 0) {
+      console.log(
+        "Data exists on Twitter account MYCOVIDCasesBot. Skipping tweet proccess."
+      );
+    } else {
+      console.log("Tweeting tweet 1 of 2...");
+      await twitterClient.v2.tweet(tweet1);
+      console.log("Tweeting tweet 2 of 2...");
+      await twitterClient.v2.tweet(tweet2);
+      console.log("Successful");
+    }
+  } catch (ApiResponseError) {
+    if (ApiResponseError.rateLimit) {
+      console.log(
+        `Rate limited: reset on ${ApiResponseError.rateLimit.reset} (epoch)`
+      );
+    }
   }
 };
 
