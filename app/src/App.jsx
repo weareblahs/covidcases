0c67cda237259e7f95bab377d3916c7e9c3f1086
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2025-05-02 19:17:36 +0800

    first react-based realtime version powered by data.gov.my

---
+++ app/src/App.jsx
@@ -0,0 +1,96 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+import { getData } from "./getData";
+function App() {
+  const [data, setData] = useState({ date: "...", cases_new: "..." });
+  useEffect(() => {
+    async function get() {
+      const caseData = await getData();
+      if (caseData) {
+        if (caseData.data.filter((d) => d.state == "Malaysia").length != 0) {
+          setData(
+            caseData.data.filter((d) => d.state == "Malaysia")[
+              caseData.data.filter((d) => d.state == "Malaysia").length - 1
+            ]
+          );
+        }
+      }
+      // process data - only get the latest malaysia data since this is for national cases
+    }
+    get();
+  }, []);
+
+  console.log(data);
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+      }}
+    >
+      <div class="centertextv2">
+        <h1
+          style={{
+            fontSize: "3em",
+          }}
+        >
+          <center>COVID-19 Cases in Malaysia as of {data.date}</center>
+        </h1>
+      </div>
+      <div class="center-center">
+        <h1>{data.cases_new}</h1>
+      </div>
+      <h2>
+        <center>
+          {/* <a
+            href="https://weareblahs.github.io/covidcases/state/RURL.html"
+            class="redbox"
+          >
+            {" "}
+            View REV{" "}
+          </a> */}
+        </center>
+      </h2>
+      <h2>
+        {/* <center>
+          <a href="https://weareblahs.github.io/covidcases/navi" class="redbox">
+            {" "}
+            More statistics{" "}
+          </a>
+        </center> */}
+      </h2>
+      <h2>
+        <center>
+          {/* <a
+            href="https://weareblahs.github.io/covidcases/img/TYPE_STATENAME.jpg"
+            download="COVID-19_TYPE_in_STATENAME_DOWNLOAD_DATE.jpg"
+            class="redbox"
+          >
+            {" "}
+            Save as image{" "}
+          </a> */}
+        </center>
+      </h2>
+      <h2>
+        <center>
+          This website is available on Twitter / X as a bot, which posts updates
+          whenever there is an update on cases.{" "}
+          <a href="https://twitter.com/mycovidcasesbot" class="sociallink">
+            <u>Follow us on Twitter</u>
+          </a>
+        </center>
+      </h2>
+      <h3>
+        <center>Realtime version. Updates when you refresh.</center>
+        <center>State cases coming soon.</center>
+        <center>
+          <a href="https://github.com/weareblahs/covidcases">
+            <u>Source code</u>
+          </a>
+        </center>
+      </h3>{" "}
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/getData.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const getData = async () => {
+  var today = new Date();
+  let data = [];
+  while (data.length == 0) {
+    today.setDate(today.getDate() - 10);
+    var formatted = today.toJSON().slice(0, 10);
+    const res = await axios.get(
+      `https://api.data.gov.my/data-catalogue/?id=covid_cases&date_start=${formatted}@date`
+    );
+    if (data.length == 0) {
+      console.log(`No data for ${formatted}`);
+    }
+    data = res.data;
+    if (data.length != 0) {
+      console.log(
+        `Data found for ${formatted}. Returning data for current function...`
+      );
+    }
+  }
+  return { date: today, data };
+};
